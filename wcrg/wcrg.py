72ce012b38d6727dfb55c3a74e2e1e3858f10465
Ashley Jameson <ash.jayanthy@gmail.com>
2022-07-13 20:23:57 +0530

    .

--- wcrg/wcrg.py
+++ wcrg/wcrg.py
@@ -1,1 +1,12 @@
 import numpy as np
+import pywavelets
+
+# for d = 2 only
+
+class EstimateEnergy():
+    def __init__(self,phi_0, num_j, a=1):
+        self.phi_0 = phi_0
+        self.j = np.arange(0,num_j)
+
+    def wavelet_field(self):
+        self.phi_0_bar = 
