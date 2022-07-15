0b0d2b407291baf52593e97d23314c5e034dee96
Ashley Jameson <ash.jayanthy@gmail.com>
2022-07-15 22:41:52 +0530

    new directions

--- wcrg/wcrg.py
+++ wcrg/wcrg.py
@@ -1,12 +1,13 @@
 import numpy as np
-import pywavelets
+import pywt
 
 # for d = 2 only
 
 class EstimateEnergy():
     def __init__(self,phi_0, num_j, a=1):
         self.phi_0 = phi_0
-        self.j = np.arange(0,num_j)
+        self.phi_array = np.zeros((num_j,2))
+        self.a = a
 
     def wavelet_field(self):
-        self.phi_0_bar = 
+        self.phi_0_bar =
